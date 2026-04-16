c88db464e2ddf7d5e957868fc765834076cd4213
jvnc3d <142265048+jvnc3d@users.noreply.github.com>
2026-04-16 15:33:51 -0300

    Update sw.js

--- api/sw.js
+++ api/sw.js
@@ -1,1 +1,8 @@
-
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => clients.claim());
+self.addEventListener('fetch', e => {
+  if (e.request.url.includes('/api/')) return;
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
